011721faab03dffa9429e7e200715282327e40a4
mhkeller <code@mhkeller.com>
2017-02-14 20:20:06 -0500

    add example files

---
+++ example.js
@@ -0,0 +1,29 @@
+// This is a comment
+
+function Person (id) {
+  var self = this
+  self.id = id
+}
+
+Person.prototype.initialize = function () {
+  var names = ['hello', 'goodbye']
+  var index = Math.round(Math.random())
+  this.name = names[index]
+}
+
+Person.prototype.greet = function () {
+  var options = {
+    coming: 'hello',
+    going: 'goodbye'
+  }
+  var index = Math.round(Math.random())
+  var key = Object.keys(options)[index]
+  return options[key]
+}
+
+var person1 = new Person(1)
+console.log(person1.greet().coming)
+console.log(person1.greet().going)
+
+
+module.exports = Person
