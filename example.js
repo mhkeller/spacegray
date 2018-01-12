1ec25865628637ce4e58570e1a8cf5ad1aef4928
mhkeller <code@mhkeller.com>
2018-01-11 20:59:24 -0500

    update example.js

--- example.js
+++ example.js
@@ -1,5 +1,9 @@
 // This is a comment
 
+var module = require('my-module')
+
+import * as myModule from 'my-module'
+
 function Person (id) {
   var self = this
   self.id = id
@@ -25,5 +29,7 @@ var person1 = new Person(1)
 console.log(person1.greet().coming)
 console.log(person1.greet().going)
 
-
 module.exports = Person
+
+// es6 module
+export default myModule
